c4bf57c77aa223699db9f4475844f0a86a737597
Ishini <misnfernando1@gmail.com>
2025-07-16 00:06:51 +0530

    Registration form created

---
+++ reg_form.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Registration Form</title>
+    </head>
+    <body>
+        <h2>JOB SEEKER REGISTRATION FORM</h2>
+        <h3>Registration Details</h3>
+        <form>
+            Title:
+                <input type="text" name="titel">
+                <br><br>
+            Name:
+                <input type="text" name="name">
+                <br><br>
+            Gender:
+                <input type="radio" value="male">male               
+                <input type="radio" value="female">female
+                <br><br>
+            Date of Birth:
+                <input type="date">
+                <br><br>
+            <input type="submit" value="Sign Up">
+            <input type="reset" value="reset">
+        </form>
+    </body>
+</html>
