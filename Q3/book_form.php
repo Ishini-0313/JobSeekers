b7500dbb72471dbc2af4eae01ec8d8a5fd31f7fa
Ishini <misnfernando1@gmail.com>
2025-08-21 22:20:59 +0530

    done

---
+++ Q3/book_form.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book Form</title>
+</head>
+<body>
+    <form action="book_form.php" method ="post">
+        <h2>Enter Book Details</h2>
+
+        Book Id :
+        <input type="int" name="id">
+        <br><br>
+
+        Book Author :
+        <input type="text" name="author">
+        <br><br>
+
+        Book Name :
+        <input type="text" name="name">
+        <br><br>
+
+        Book ISBN no :
+        <input type="int" name="no">
+        <br><br>
+
+    </form>
+</body>
+</html>
